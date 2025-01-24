2b23aa8cedb31f11bf22433d7eb7768fce9371e5
Daniel Owen <unlimiteddemi@gmail.com>
2025-01-24 10:44:49 +0100

    trying to fix browser history

--- src/router/index.js
+++ src/router/index.js
@@ -9,8 +9,8 @@ const router = createRouter({
   routes: [
     
     {
-      path: "/",
-      name: "LandingPage",
+      path: "",
+      name: "/LandingPage",
       component : LandingPage
     },
     
